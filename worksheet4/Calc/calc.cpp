029e753a9a13ddd5da00a439cde698010c72e5c8
xuankaimichelezhang <xuankai_michele_zhang@hotmail.com>
2026-02-12 10:44:03 +0000

    Implement Worksheet 4 Part 1 and 3 scaffolding

---
+++ worksheet4/Calc/calc.cpp
@@ -0,0 +1,25 @@
+#include "adder.h"
+#include <iostream>
+#include <sstream>
+
+
+int main(int argc, char *argv[]) {
+  int a, b, c;
+
+  if (argc != 3)
+    return 1;
+
+  std::string sa(argv[1]);
+  std::string sb(argv[2]);
+
+  std::stringstream ssa(sa);
+  std::stringstream ssb(sb);
+
+  ssa >> a;
+  ssb >> b;
+
+  c = add(a, b);
+  std::cout << c;
+
+  return 0;
+}

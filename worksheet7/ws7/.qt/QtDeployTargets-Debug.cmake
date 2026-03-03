e659c70a5d08206190c813eaf8b8e669c288a36f
xuankaimichelezhang <xuankai_michele_zhang@hotmail.com>
2026-03-03 15:16:41 +0000

    Delete redundant ws7 directory

--- worksheet7/ws7/.qt/QtDeployTargets-Debug.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ws6_FILE C:/Users/LLR User/2026_20654922/worksheet6/ws6/Debug/ws6.exe)
-set(__QT_DEPLOY_TARGET_ws6_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ws6_RUNTIME_DLLS C:/Qt/6.10.2/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Cored.dll)
--- worksheet7/ws7/.qt/deploy_ws6_766dd48ea8-MinSizeRel.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/LLR User/2026_20654922/worksheet6/ws6/.qt/QtDeploySupport-MinSizeRel.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/ws6-plugins-MinSizeRel.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/LLR User/2026_20654922/worksheet6/ws6/MinSizeRel/ws6.exe"
-    GENERATE_QT_CONF
-)
